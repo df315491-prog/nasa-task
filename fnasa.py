386b034e94cb23541c62b74e81eb9322a9a885d7
f46271 <f46271@gmail.com>
2025-11-14 11:08:55 +0000

    added_api_key

--- fnasa.py
+++ fnasa.py
@@ -2,11 +2,10 @@ import requests
 import datetime
 import pprint
 
-API_KEY = "DEMO_KEY"   # Replace with your own key if you have one
+API_KEY = "ibao9Xte7MmWeJG8TdVq07bfA85pNKWFeTUzwwwA"   
 ENDPOINT = "https://api.nasa.gov/neo/rest/v1/feed"
 
-# The feed endpoint supports up to 7 days per request
-# We'll pull 4 weeks (28 days) in 7-day chunks
+
 today = datetime.date.today()
 start_date = today - datetime.timedelta(days=28)
 
@@ -30,6 +29,5 @@ while chunk_start < today:
     all_data.append(data)
     chunk_start = chunk_end + datetime.timedelta(days=1)
 
-# Pretty-print the raw data (list of 4 JSON payloads)
 pp = pprint.PrettyPrinter(indent=2)
 pp.pprint(all_data)
