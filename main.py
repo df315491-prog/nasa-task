73f3c6f43c8f676dd6594fd52766f1377b602855
f46271 <f46271@gmail.com>
2025-11-14 11:18:55 +0000

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=ibao9Xte7MmWeJG8TdVq07bfA85pNKWFeTUzwwwA"
-
-response = requests.get(url)
-data = response.json()
-
-print(data)
